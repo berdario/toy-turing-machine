ed965d089bf346366ad5c1b627175c35992ef6e2
Dario Bertini <berdario@gmail.com>
2012-06-28 19:59:26 +0200

    Old changes I forgot to commit: added my exam's solution and 2 other cases

--- tm.py
+++ tm.py
@@ -156,5 +156,110 @@ class Sum(AbstractTest, TestCase):
 	def check_output(self, inp):
 		self.assertEqual(sum(inp), *self.tm.get_output())
 
+class Fc1(AbstractTest, TestCase):
+	# instructions = [
+	# 	(1,1,1,1,2),
+	# 	(2,0,1,1,0),
+	# 	(2,1,0,1,3),
+	# 	(3,0,0,1,0),
+	# 	(3,1,1,-1,4),
+	# 	(4,0,1,1,5),
+	# 	(5,1,0,1,5),
+	# 	(5,0,0,1,0),
+	# 	]
+	instructions = [
+		(1,1,1,1,2),
+		(2,0,1,1,0),
+		(2,1,1,1,3),
+		(3,0,0,-1,4),
+		(3,1,0,1,4),
+		(4,1,0,1,4),
+		(4,0,0,0,0),
+		]
+	inputs = range(20)
+	
+	def check_output(self, inp):
+		if inp == 1:
+			self.assertEqual(0, *self.tm.get_output())
+		else:
+			self.assertEqual(1, *self.tm.get_output())
+
+# class SwapMul2(AbstractTest, TestCase):
+# 	instructions = [
+# 		(1,1,0,1,9),
+# 		(9,0,0,1,10),
+# 		(9,1,1,1,2),
+# 		(2,1,1,1,2),
+# 		(2,0,0,1,3),
+# 		(3,1,1,1,3),
+# 		(3,0,0,1,4),
+# 		(4,0,1,1,5),
+# 		(4,1,1,1,4),
+# 		(5,0,1,-1,6),
+# 		(6,1,1,-1,6),
+# 		(6,0,0,-1,7),
+# 		(7,1,1,-1,7),
+# 		(7,0,0,-1,8),
+# 		(8,1,1,-1,8),
+# 		(8,0,0,1,1),
+# 		(10,1,1,1,10),
+# 		(10,0,0,1,11),
+# 		(11,0,1,1,12),
+# 		(11,1,1,1,11),
+# 		(12,0,1,-1,0),
+# 		]
+# 	inputs = prepare_tuple_list()
+	
+# 	def check_output(self, inp):
+# 		self.assertEqual((inp[1], 2*inp[0]), self.tm.get_output())
+
+class testF(AbstractTest, TestCase):
+	instructions = [
+		(1,1,0,1,2),
+		(2,1,0,1,3),
+		(3,1,0,1,4),
+		(4,1,0,1,5),
+		(5,0,0,1,8),
+		(3,0,1,0,0),
+		(4,0,0,1,6),
+		(6,1,1,1,6),
+		(6,0,0,1,7),
+		(7,1,0,1,7),
+		(7,0,0,0,0),
+		(8,1,0,1,8),
+		(8,0,0,0,0),
+		]
+	# instructions = [
+	# 	(1,1,0,1,2),
+	# 	(2,1,0,1,3),
+	# 	(3,0,1,0,0),
+	# 	(3,1,0,1,4),
+	# 	(4,0,0,1,5),
+	# 	(5,1,1,1,5),
+	# 	(5,0,0,1,6),
+	# 	(6,1,0,1,6),
+	# 	(6,0,0,0,0),
+	# 	(4,1,0,1,7),
+	# 	(7,0,0,1,8),
+	# 	(8,1,0,1,8),
+	# 	(8,0,0,0,0),
+	# 	]
+	inputs = [(1,0), (1,1)]+[(1,randint(0,22)) for i in range(10)] + \
+		[(2,0,0), (2,0,1), (2,1,0), (2,1,1)]+[(2,randint(0,22), randint(0,22)) for i in range(10)] + \
+		[(3,0,0,0), (3,0,1,1), (3,1,0,0), (3,1,1,1)]+[(3,randint(0,22), randint(0,22), randint(0,22)) for i in range(10)]
+	
+	def check_output(self, inp):
+		self.assertEqual(self.f(inp), self.tm.get_output())
+
+	def f(self, inp):
+		x, y = inp[0], inp[1:]
+		if x==1:
+			if len(y)!=1: raise Exception()
+			return (y[0]+1,)
+		elif x==2:
+			return (y[0],)
+		elif x==3:
+			return (y[1], y[2])
+
 if __name__ == "__main__":
 	main()
